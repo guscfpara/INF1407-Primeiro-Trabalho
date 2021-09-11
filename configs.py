eecca9bf3f1f02a47442a6e7fa9d05f057e2ab7f
Gustavo Paraguassu <guparagua@gmail.com>
2021-09-11 11:00:39 -0300

    Remove Django and

---
+++ configs.py
@@ -0,0 +1,22 @@
+### Config File
+web_server_config = {
+    # Lista de arquivos default
+    "defaults_files" : ["dog.gif","golden.jpg","index.html","javascript.js","labrador.png"],
+    # Porta que será escutada
+    "port" : 8080,
+    # Diretório Local
+    "local_dir" : "views",
+    # Página de Erro
+    "error_file" : "404.html",
+    # Char codification
+    "code" : "iso-8859-1"
+}
+
+### Config File Example
+# web_server_config = {
+#     "defaults_files" : ["dog.gif","golden.jpg","index.html","javascript.js","labrador.png"],
+#     "port" : 8080,
+#     "local_dir" : "views",
+#     "error_file" : "404.html"
+#     "code" : "iso-8859-1"
+# }
